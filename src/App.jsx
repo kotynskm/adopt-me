3a8c4986e71d2e687d184d56c83e3354c39e7d6c
Kailey <k.kotynski@gmail.com>
2022-11-23 21:36:10 -0800

    Edited eslint, converted App and Pet components to use jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,20 @@
-import React from "react";
 // import ReactDOM from "react-dom";
 // will import only createRoot instead of ReactDOM since we are only using createRoot
 import { createRoot } from "react-dom/client";
-
-// make pet component
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.breed),
-    React.createElement("h2", {}, props.animal),
-  ]);
-};
+import Pet from "./Pet";
 
 // create App
 const App = () => {
-  // create a div element with an h1 child element with text "Adopt Me!"
-  // NO ONE WRITES REACT LIKE THIS ANYMORE - WE USE JSX, but this is what it looks liked rendered out.
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    // create some pet elements
-    React.createElement(Pet, {
-      name: "Luna",
-      breed: "Short Hair",
-      animal: "Cat",
-    }),
-    React.createElement(Pet, {
-      name: "Felix",
-      breed: "Russian Blue",
-      animal: "Cat",
-    }),
-    React.createElement(Pet, {
-      name: "Mango",
-      breed: "Caique",
-      animal: "Bird",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Golden Retriever" />
+      <Pet name="Mango" animal="Bird" breed="Caique" />
+      <Pet name="Felix" animal="Cat" breed="Short Hair Tabby" />
+    </div>
+  );
 };
 // get the div with the ID root, create the root using this container, render our App
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);

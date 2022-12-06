5860fd3229cd6091377ec99053e854c5b9260e59
Kailey <k.kotynski@gmail.com>
2022-12-06 11:09:00 -0800

    Added image rotation to carousel on click

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -10,6 +10,15 @@ class Carousel extends Component {
   static defaultProps = {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
+
+  // update active item on carousel
+  handleIndexClick = (e) => {
+    this.setState({
+      // + makes a str into a number, which we need to have the index as a number
+      // dataset refers to all data- things on an object
+      active: +e.target.dataset.index,
+    });
+  };
   // class components always have a render function
   render() {
     const { active } = this.state; // keep track of internal state
@@ -20,7 +29,10 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal hero" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
+              data-index={index}
               key={photo}
               src={photo}
               className={index === active ? "active" : ""}
